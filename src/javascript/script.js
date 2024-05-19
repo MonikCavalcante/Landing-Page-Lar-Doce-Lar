3fbfc0e839da131db9f58b7ccb0fa07adad9f998
MonikCavalcante <monikcavalcante1801@gmail.com>
2024-05-19 18:43:00 -0300

    first commit

---
+++ src/javascript/script.js
@@ -0,0 +1,74 @@
+/* faz o menu do mobile abrir */
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+
+    /* sombra da navbar */
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function (){
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() -  header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if(scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.5');
+        }
+
+        sections.each(function(i){
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        });
+
+        /* add o efeito de passar a linha na navbar */
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+        
+    });
+
+    /* faz a animação de efeito arrasta */
+    ScrollReveal().reveal('#cta',{
+        origin: 'left', 
+        duration: 2000,
+        distance:'20%'
+    });
+    ScrollReveal().reveal('#banner',{
+        origin: 'right', 
+        duration: 2000,
+        distance:'20%'
+    });
+    ScrollReveal().reveal('.dish',{
+        origin: 'left', 
+        duration: 2000,
+        distance:'20%'
+    });
+    ScrollReveal().reveal('#testimonial_chef',{
+        origin: 'left', 
+        duration: 1000,
+        distance:'20%'
+    });
+    ScrollReveal().reveal('#feedbacks',{
+        origin: 'right', 
+        duration: 1000,
+        distance:'20%'
+    });
+    ScrollReveal().reveal('#mobile_menu.active',{
+        origin: 'right', 
+        duration: 1000,
+        distance:'20%'
+    });
+});
+
+
